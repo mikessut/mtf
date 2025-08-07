768d90bba1617078715d59cce9a597bdad127b0f
Mike Sutton <mikesutton@gmail.com>
2025-08-07 13:20:12 -0600

    roi

--- src/mtf/__main__.py
+++ src/mtf/__main__.py
@@ -9,9 +9,24 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('input_image')
+    parser.add_argument('--gui-roi', action='store_true', default=False)
+    parser.add_argument('--roi', help="Specify in quotes as you would slice a matrix. E.g. \"200:400, 50:150\"")
     args = parser.parse_args()
 
     img = np.array(Image.open(args.input_image))
+
+    if args.gui_roi:
+        f = plt.figure()
+        plt.imshow(img)
+        tmp = np.array(plt.ginput(2))
+        args.roi = f"{tmp[:, 1].min().round().astype(int)}:{tmp[:, 1].max().round().astype(int)}, {tmp[:, 0].min().round().astype(int)}:{tmp[:, 0].max().round().astype(int)}"
+        print(f"ROI set to: \"{args.roi}\"")
+
+    if args.roi is not None:
+        print(f"Cropping to roi of \"{args.roi}\"")
+        row, col = args.roi.split(",")
+        roi_slice = (slice(*map(int, row.split(":"))), slice(*map(int, col.split(":"))))
+        img = img[roi_slice]
 
     line = mtf.fit_line(img)
     x, y, dy = mtf.img_to_LSF(img, line)
